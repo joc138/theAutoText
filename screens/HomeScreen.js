b725aa57e9436632a225fbdd8fe94f1f24e1a98a
Jonathan Cervantes <joc138@ucsd.edu>
2019-07-19 21:43:37 -0700

    Made plus button go to a second page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet, TouchableHighlight } from 'react-native';
 const HomeScreen = ( {navigation }) => (
-	<View>
-		<Text> Home Screen </Text>	
-		<Button onPress={() => navigation.navigate('Set')} title="click me" />
+      	<View style={styles.container}>
+		<TouchableHighlight style={styles.button} onPress={() => navigation.navigate('Set')} underlayColor= '#f5f'>
+		  <Text style={styles.buttonText}>+</Text>
+		</TouchableHighlight>
 	</View>
 	
 )
+const styles = StyleSheet.create({
+	button: {
+		position: 'absolute',
+		bottom: 30,
+		right: 30,
+		width: 90,
+		height: 90,
+		alignItems: 'center',
+		backgroundColor: '#05a5d1',
+		borderRadius: 175,
+	},
+	buttonText: {
+		fontSize: 90,
+		flex: 1,
+		bottom: 15,
+		justifyContent: 'center',
+		alignItems: 'center',
+		color: 'white',
+	},
+  	container: {
+     		flex: 1,
+    		justifyContent: 'center',
+  		alignItems: 'center',
+   		 backgroundColor: '#abcdef',
+  	},
+});
 export default HomeScreen
